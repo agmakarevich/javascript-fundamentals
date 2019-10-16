3c53cecc2f866a690abb14ae384ed77f546dd637
Max Makarevich <M.Makarevich@sam-solutions.com>
2019-10-17 00:07:54 +0300

    part-1.4

--- string.test.js
+++ string.test.js
@@ -1,5 +1,11 @@
 describe("Strings", () => {
   it("Should join two strings with a space.", () => {
+    function combine1(a, b){
+      return a + " " + b
+    }
+    function combine2(a, b){
+      return [a, b].join(" ")
+    }
     // TODO: write 2 function with different way for join string
 
     expect(combine1("hello", "world")).toBe("hello world");
@@ -7,42 +13,57 @@ describe("Strings", () => {
   });
 
   it("Should get string length", () => {
-    expect(/* getLength("") */).toBe(0);
-    expect(/* getLength("word") */).toBe(4);
+    function getLength(a){
+      return a.length
+    }
+    expect(getLength("")).toBe(0);
+    expect(getLength("word")).toBe(4);
   });
 
   it("Should create greeting message from template", () => {
-    expect(/* greeting("Ivan") */).toBe("Hello, Ivan!");
+    function greeting(a){
+      return `Hello, ${a}!`
+    }
+    expect(greeting("Ivan")).toBe("Hello, Ivan!");
   });
 
   it("Should strip leading and trailing spaces from a string", () => {
-    expect(/* ??? ' aaaa bbb   ' */).toBe("aaaa bbb");
+    expect(' aaaa bbb   '.trim()).toBe("aaaa bbb");
   });
 
   it("Replace all word occurence in the sentences", () => {
-    expect(/*  ??? 'aaa bbb ccc aaa  bb', 'aaa', 'eeeee' */).toBe(
+    expect('aaa bbb ccc aaa bb'.replace(/aaa/g, 'eeeee')).toBe(
       "eeeee bbb ccc eeeee bb"
     );
   });
 
   it("Should validate string length", () => {
-    expect(/* validateLength('abcde', 1, 5)*/).toBe(true);
-    expect(/* validateLength('a', 1, 5)*/).toBe(true);
-    expect(/* validateLength('ab', 1, 5)*/).toBe(true);
-    expect(/* validateLength('', 1, 5)*/).toBe(false);
-    expect(/* validateLength('abcdef', 1, 5)*/).toBe(false);
+    function validateLength(a, b, c){
+      return a.length >= b && a.length <= c
+    }
+    expect(validateLength('abcde', 1, 5)).toBe(true);
+    expect(validateLength('a', 1, 5)).toBe(true);
+    expect(validateLength('ab', 1, 5)).toBe(true);
+    expect(validateLength('', 1, 5)).toBe(false);
+    expect(validateLength('abcdef', 1, 5)).toBe(false);
   });
 
   it("Should do case insensitive strings comparison", () => {
-    expect(/* compare('aBc', 'ABC') */).toBe(true);
-    expect(/* compare('abc' 'abc') */).toBe(true);
+    function compare(a, b){
+      return a.toLowerCase() === b.toLowerCase()
+    }
+    expect(compare('aBc', 'ABC')).toBe(true);
+    expect(compare('abc', 'abc')).toBe(true);
   });
 
   it("Should trim string according symbols limit", () => {
-    expect(/* trim('Lorem ipsum dolor sit amet', 7) */).toBe("Lorem i");
-    expect(/* trim('Lorem ipsum dolor sit amet', 12) */).toBe("Lorem ipsum");
-    expect(/* trim('Lorem ipsum dolor sit amet', 11) */).toBe("Lorem ipsum");
-    expect(/* trim('Lorem ipsum dolor sit amet', 100) */).toBe(
+    function trim(a, b){
+      return a.slice(0, b)
+    }
+    expect(trim('Lorem ipsum dolor sit amet', 7)).toBe("Lorem i");
+    expect(trim('Lorem ipsum dolor sit amet', 12)).toBe("Lorem ipsum ");
+    expect(trim('Lorem ipsum dolor sit amet', 11)).toBe("Lorem ipsum");
+    expect(trim('Lorem ipsum dolor sit amet', 100)).toBe(
       "Lorem ipsum dolor sit amet"
     );
   });
